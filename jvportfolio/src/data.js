b8e2cd020ba1584eabd5cb2f70ec7fccfe95b355
JVelezFD <jvelez117@gmail.com>
2022-09-23 08:52:22 -0400

    simplified the site reduced the number of animations and removed darkmode error I was gettting

---
+++ jvportfolio/src/data.js
@@ -0,0 +1,48 @@
+export const projects = [
+  {
+    title: "Meal Finder",
+    subtitle: "Material UI",
+    description:
+      "App will help users find a meal to cook based on option chosen: breakfast, lunch, dinner",
+    image: "./proj1.png",
+    link: "https://github.com/JVelezFD/MealFinder",
+  },
+  {
+    title: "eSport Profile Maker",
+    subtitle: "Node and MySQL",
+    description:
+      "The app will allow users to create esports profiles to share with potential recruiters for collegiate esports scholarship opportunities",
+    image: "./proj2.png",
+    link: "https://github.com/gabeab34/Smash.ME",
+  },
+]
+
+  // {
+  //   title: "project 3 soon",
+  //   subtitle: "",
+  //   description:
+  //     "",
+  //   image: "",
+  //   link: "",
+  // },
+//   {
+//     title: "project 4 soon",
+//     subtitle: "",
+//     description:
+//       "",
+//     image: "",
+//     link: "",
+//   },
+// ];
+
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node",
+  "GraphQL",
+  "Material UI",
+  "MongoDB",
+  "MySQL",
+];
