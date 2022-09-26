41bb78cec7eacb3fab1b354eb15542633a45e82f
JVelezFD <jvelez117@gmail.com>
2022-09-26 16:04:44 -0400

    added background color to fill white space

--- src/App.js
+++ src/App.js
@@ -4,25 +4,23 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 export default function App() {     
   return (
-  <main className="text-goldGolden bg-richBlack body-font">
+  <main className="text-goldGolden bg-richBlack bg-inherit body-font">
     <Router>
      <Navbar />
-       <Routes>
-       
+       <Routes>       
         <Route path ="/about" element={<About/>}/>
         <Route path ="/projects" element={<Projects/>} />
         <Route path ="/skills" element={<Skills/>}/>
         <Route path ="/contact" element={<Contact/>}/> 
         <Route path ="/" exact element={<About/>}/>
         </Routes>
-     
-    
-    </Router>
+    </Router>   
     </main> 
   );
 }
