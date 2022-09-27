4b6ea3ef2929926cbce0d587e2a9a1784b56c95f
JVelezFD <jvelez117@gmail.com>
2022-09-26 21:19:39 -0400

    added dependencies and script for deployed app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+
 import { ChipIcon } from "@heroicons/react/solid";
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ import { Link } from "react-router-dom"
 
 export default function Footer() {
   return (
-    <footer className="bg-oxfordBlue md:sticky bottom-8 z-10">
+    <footer className="bg-oxfordBlue md:sticky bottom-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <nav className="md:mr-auto md:ml-4 md:py-0 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-goldGolden justify-center">
+        <div className="md:mr-auto md:ml-4 md:py-0 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-goldGolden justify-center">
         <div>         
           <a
            href="https://www.linkedin.com/in/j-velez-fiserv/" className="mr-5 ">
@@ -25,15 +25,9 @@ export default function Footer() {
             src="./github.png"></img>           
           </a>
           </div>  */}
-        </nav>
+        </div>
       
-        
-      <Link to= "/contact">
-        <a className="inline-flex items-center bg-oxfordBlue border-0 py-1 px-3 focus:outline-none hover:bg-prussianBlue rounded text-goldGolden mt-4 md:mt-0">
-          Contact Me
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
-        </a> 
-      </Link>
+   
         
       </div>
     </footer>
